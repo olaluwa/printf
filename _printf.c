084d042b1dd567985a8dc7861e19f9126eb7cfbc
olaluwa <oluwapelumiola3@gmail.com>
2022-04-20 00:28:46 +0100

    print your f project

--- _printf.c
+++ _printf.c
@@ -1,106 +1,50 @@
 #include "main.h"
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
- *
- * Return: number of chars printed.
- *
-
-int _printf(const char *format, ...)
-{
-        unsigned int i = 0, len = 0, ibuf = 0;
-        va_list arguments;
-        int (*function)(va_list, char *, unsigned int);
-        char *buffer;
-
-        va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-        if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-                return (-1);
-        if (!format[i])
-                return (0);
-        for (i = 0; format && format[i]; i++)
-        {
-                if (format[i] == '%')
-                {
-                        if (format[i + 1] == '\0')
-                        {       print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-                                return (-1);
-                        }
-                        else
-                        {       function = get_print_func(format, i + 1);
-                                if (function == NULL)
-                                {
-                                        if (format[i + 1] == ' ' && !format[i + 2])                                                return (-1);
-                                        handl_buf(buffer, format[i], ibuf), len++, i--;
-                                }
-                                else
-                                {
-                                        len += function(arguments, buffer, ibuf);
-                                        i += ev_print_func(format, i + 1);
-                                }
-                        } i++;
-                }
-                else
-                        handl_buf(buffer, format[i], ibuf), len++;
-                for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-                        ;
-        }
-        print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-        return (len);
-}
-*/
-
-
-/**
- * _write - write to stdout
- * @output: the string to write
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
  */
-void _write(buffer_t *output)
+int _printf(char *format, ...)
 {
-        write(1, output->start, output->len);
-        free_buffer(output);
-}
-
-/**
- * _printf - produces output according to a format.
- * @format: is a character string.
- * Return: the length of the format.
- */
-int _printf(const char *format, ...)
-{
-        unsigned int len = 0, i = 0;
-        unsigned int (*convert)(va_list, buffer_t *);
-        buffer_t *output;
-        va_list args;
-
-        output = init_buffer();
-        if (output == NULL)
-                return (-1);
-
-        va_start(args, format);
+        int written = 0, (*structype)(char *, va_list);
+        char q[3];
+        va_list pa;
 
         if (format == NULL)
                 return (-1);
-
-        while (*(format + i))
+        q[2] = '\0';
+        va_start(pa, format);
+        _putchar(-1);
+        while (format[0])
         {
-                if (*(format + i) == '%')
+                if (format[0] == '%')
                 {
-                        convert = handle_specifiers((format + i + 1));
-                        if (convert != NULL)
+                        structype = driver(format);
+                        if (structype)
                         {
-                                len += convert(args, output);
-                                format += 2;
-                                continue;
+                                q[0] = '%';
+                                q[1] = format[1];
+                                written += structype(q, pa);
                         }
+                        else if (format[1] != '\0')
+                        {
+                                written += _putchar('%');
+                                written += _putchar(format[1]);
+                        }
+                        else
+                        {
+                                written += _putchar('%');
+                                break;
+                        }
+                        format += 2;
                 }
-                len += _memcpy(output, (format + i), 1);
-                i++;
+                else
+                {
+                        written += _putchar(format[0]);
+                        format++;
+                }
         }
-
-        _write(output);
-        va_end(args);
-
-        return (len);
+        _putchar(-2);
+        return (written);
 }
--- main.h
+++ main.h
@@ -1,113 +1,36 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024
-
 /**
- * struct buffer - A new buffer type
- * @start: - pointer to the head of the pointer
- * @buffer: - pointer to an array of char
- * @len: - size of the buffer
- */
-typedef struct buffer
-{
-        char *buffer;
-        char *start;
-        unsigned int len;
-} buffer_t;
-
-/**
- * struct format - A new type defining a format struct.
- * @specifier: A character representing a conversion specifier.
- * @func: A pointer to a conversion function corresponding to specifier.
- */
-typedef struct format
-{
-        unsigned char specifier;
-        unsigned int (*func)(va_list, buffer_t *);
-} format_t;
-
-int _printf(const char *format, ...);
-
-/* CONVERTER FUNCTIONS */
-unsigned int convert_c(va_list args, buffer_t *output);
-unsigned int convert_s(va_list args, buffer_t *output);
-unsigned int convert_d(va_list args, buffer_t *output);
-unsigned int convert_o(va_list args, buffer_t *output);
-unsigned int c_percent(va_list args, buffer_t *output);
-unsigned int convert_b(va_list args, buffer_t *output);
-
-/* HELPERS FUNCTION */
-buffer_t *init_buffer(void);
-void free_buffer(buffer_t *buffer);
-unsigned int _memcpy(buffer_t *dest, const char *src, unsigned int n);
-unsigned int convert_base(long int number, int base, char *representation,
-buffer_t *output);
-
-/* HANDLERS */
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *);
-
-#endif /* MAIN_H */
-
-/**
- * struct print - struct for printer functions
- * @type_arg: identifier
- * @f: pointer to a printer functions
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
  *
- * Description: struct that stores pointers to a
- * printer functions.
- *
-typedef struct print
+ * Return: int
+ */
+typedef struct structprint
 {
-	char *type_arg;
-	int (*f)(va_list, char *, unsigned int);
-} print_t;
+        char *q;
+        int (*u)(char *format, va_list);
+} structype;
 
-int _printf(const char *format, ...);
-int print_prg(va_list __attribute__((unused)), char *, unsigned int);
-int print_chr(va_list arguments, char *buf, unsigned int ibuf);
-int print_str(va_list arguments, char *buf, unsigned int ibuf);
-int print_int(va_list arguments, char *buf, unsigned int ibuf);
-int print_bnr(va_list arguments, char *buf, unsigned int ibuf);
-int print_unt(va_list arguments, char *buf, unsigned int ibuf);
-int print_oct(va_list arguments, char *buf, unsigned int ibuf);
-int print_hex(va_list arguments, char *buf, unsigned int ibuf);
-int print_upx(va_list arguments, char *buf, unsigned int ibuf);
-int print_usr(va_list arguments, char *buf, unsigned int ibuf);
-int print_add(va_list arguments, char *buf, unsigned int ibuf);
-int print_rev(va_list arguments, char *buf, unsigned int ibuf);
-int print_rot(va_list arguments, char *buf, unsigned int ibuf);
-int prinlint(va_list arguments, char *buf, unsigned int ibuf);
-int prinlunt(va_list arguments, char *buf, unsigned int ibuf);
-int prinloct(va_list arguments, char *buf, unsigned int ibuf);
-int prinlhex(va_list arguments, char *buf, unsigned int ibuf);
-int prinlupx(va_list arguments, char *buf, unsigned int ibuf);
-int prinhint(va_list arguments, char *buf, unsigned int ibuf);
-int prinhunt(va_list arguments, char *buf, unsigned int ibuf);
-int prinhoct(va_list arguments, char *buf, unsigned int ibuf);
-int prinhhex(va_list arguments, char *buf, unsigned int ibuf);
-int prinhupx(va_list arguments, char *buf, unsigned int ibuf);
-int prinpint(va_list arguments, char *buf, unsigned int ibuf);
-int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
-int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
-int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
-int prinsint(va_list arguments, char *buf, unsigned int ibuf);
-int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
-int ev_print_func(const char *s, int index);
-unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
-int print_buf(char *buf, unsigned int nbuf);
-char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
-char *fill_oct_array(char *bnr, char *oct);
-char *fill_long_oct_array(char *bnr, char *oct);
-char *fill_short_oct_array(char *bnr, char *oct);
-char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
-
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 #endif
-*/
