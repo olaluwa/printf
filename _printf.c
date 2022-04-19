0042e943ac2e1ddffec83c4e2c85066709c2f90f
olaluwa <oluwapelumiola3@gmail.com>
2022-04-19 13:43:45 +0100

    new change

--- _printf.c
+++ _printf.c
@@ -5,7 +5,8 @@
  * @format: input string.
  *
  * Return: number of chars printed.
- */
+ *
+
 int _printf(const char *format, ...)
 {
         unsigned int i = 0, len = 0, ibuf = 0;
@@ -46,5 +47,60 @@ int _printf(const char *format, ...)
                         ;
         }
         print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+        return (len);
+}
+*/
+
+
+/**
+ * _write - write to stdout
+ * @output: the string to write
+ */
+void _write(buffer_t *output)
+{
+        write(1, output->start, output->len);
+        free_buffer(output);
+}
+
+/**
+ * _printf - produces output according to a format.
+ * @format: is a character string.
+ * Return: the length of the format.
+ */
+int _printf(const char *format, ...)
+{
+        unsigned int len = 0, i = 0;
+        unsigned int (*convert)(va_list, buffer_t *);
+        buffer_t *output;
+        va_list args;
+
+        output = init_buffer();
+        if (output == NULL)
+                return (-1);
+
+        va_start(args, format);
+
+        if (format == NULL)
+                return (-1);
+
+        while (*(format + i))
+        {
+                if (*(format + i) == '%')
+                {
+                        convert = handle_specifiers((format + i + 1));
+                        if (convert != NULL)
+                        {
+                                len += convert(args, output);
+                                format += 2;
+                                continue;
+                        }
+                }
+                len += _memcpy(output, (format + i), 1);
+                i++;
+        }
+
+        _write(output);
+        va_end(args);
+
         return (len);
 }
--- main.h
+++ main.h
@@ -4,6 +4,57 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * struct buffer - A new buffer type
+ * @start: - pointer to the head of the pointer
+ * @buffer: - pointer to an array of char
+ * @len: - size of the buffer
+ */
+typedef struct buffer
+{
+        char *buffer;
+        char *start;
+        unsigned int len;
+} buffer_t;
+
+/**
+ * struct format - A new type defining a format struct.
+ * @specifier: A character representing a conversion specifier.
+ * @func: A pointer to a conversion function corresponding to specifier.
+ */
+typedef struct format
+{
+        unsigned char specifier;
+        unsigned int (*func)(va_list, buffer_t *);
+} format_t;
+
+int _printf(const char *format, ...);
+
+/* CONVERTER FUNCTIONS */
+unsigned int convert_c(va_list args, buffer_t *output);
+unsigned int convert_s(va_list args, buffer_t *output);
+unsigned int convert_d(va_list args, buffer_t *output);
+unsigned int convert_o(va_list args, buffer_t *output);
+unsigned int c_percent(va_list args, buffer_t *output);
+unsigned int convert_b(va_list args, buffer_t *output);
+
+/* HELPERS FUNCTION */
+buffer_t *init_buffer(void);
+void free_buffer(buffer_t *buffer);
+unsigned int _memcpy(buffer_t *dest, const char *src, unsigned int n);
+unsigned int convert_base(long int number, int base, char *representation,
+buffer_t *output);
+
+/* HANDLERS */
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *);
+
+#endif /* MAIN_H */
 
 /**
  * struct print - struct for printer functions
@@ -12,7 +63,7 @@
  *
  * Description: struct that stores pointers to a
  * printer functions.
- */
+ *
 typedef struct print
 {
 	char *type_arg;
@@ -59,3 +110,4 @@ char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 
 #endif
+*/
